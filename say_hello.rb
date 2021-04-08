97ed813b96a73983b5bac17f9667875944ae5334
dieter long <dieterwaitforitlong@gmail.com>
2021-04-08 03:52:45 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,7 @@
 # Build your say_hello method here
-def say_hello(name = "Hello Ruby Programmer!")
+def say_hello( name = "Hello Ruby Programmer!")
   puts "Hello, #{name}!"
 end
+
+puts say_hello(name)
+
